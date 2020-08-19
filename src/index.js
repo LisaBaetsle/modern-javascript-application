74e1d992221458ccaf42826ac1a355ea3a2c9d04
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-08-19 13:35:07 +0200

    Apply webpack on script

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {handleSubmitButton} from '../modules/submit.js';
+
+//When submit button is clicked
+document.getElementById('submit').addEventListener('click', handleSubmitButton)
+
+// When enter is pressed in submit button
+document.getElementById('cityInput').addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    handleSubmitButton();
+  }
+});
